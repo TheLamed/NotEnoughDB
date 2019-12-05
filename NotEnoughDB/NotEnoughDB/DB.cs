07c127ef94d23d9383f2be37579bf7234a8a0460
TheLamed <56411527+TheLamed@users.noreply.github.com>
2019-12-05 20:01:42 +0200

    this shit has started

---
+++ NotEnoughDB/NotEnoughDB/DB.cs
@@ -0,0 +1,27 @@
+﻿using NotEnoughDB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotEnoughDB
+{
+    public enum DataBases { SQLite }
+    public static class DB
+    {
+        public static IController GetController(DataBases db)
+        {
+            switch (db)
+            {
+                case DataBases.SQLite: return new SQLiteControl();
+
+
+
+
+
+                default: return null;
+            }
+        }
+    }
+}
